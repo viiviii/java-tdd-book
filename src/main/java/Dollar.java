89fcda9a3b8a4c17b13b405285d833c3eb8aa2c7
viiviii <viiiix.viiviii@gmail.com>
2021-04-01 23:12:02 +0900

    Fix compile-error for quickly test pass

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -5,7 +5,8 @@ class Dollar {
         this.amount = amount;
     }
 
-    void times(int multiplier) {
+    Dollar times(int multiplier) {
         amount *= multiplier;
+        return null;
     }
 }
