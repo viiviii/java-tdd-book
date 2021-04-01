3a51e99cc0df83206ec4612d9e227cbeddc34b8b
viiviii <viiiix.viiviii@gmail.com>
2021-04-01 23:43:52 +0900

    Remove unnecessary temp variables

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -6,10 +6,8 @@ public class DollarTest {
     @Test
     void testMultiplication() {
         Dollar five = new Dollar(5);
-        Dollar product = five.times(2);
-        assertEquals(new Dollar(10), product.amount);
-        product = five.times(3);
-        assertEquals(new Dollar(15), product.amount);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
     }
 
     @Test
