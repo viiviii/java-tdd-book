25bf0e3e49614792977151680affd3e7b76464b6
viiviii <viiiix.viiviii@gmail.com>
2021-04-01 22:31:03 +0900

    Add tesMultiplication()

---
+++ src/test/java/DollarTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DollarTest {
+    @Test
+    void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+}
