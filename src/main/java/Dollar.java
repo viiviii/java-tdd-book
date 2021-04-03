53b3f3e504296ba4e1ca069899361f6fe8370909
viiviii <viiiix.viiviii@gmail.com>
2021-04-03 17:10:19 +0900

    Make two currency() codes the same

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -1,7 +1,9 @@
 class Dollar extends Money {
+    private String currency;
 
     Dollar(int amount) {
         this.amount = amount;
+        currency = "USD";
     }
 
     Money times(int multiplier) {
@@ -9,6 +11,6 @@ class Dollar extends Money {
     }
 
     String currency() {
-        return "USD";
+        return currency;
     }
 }
