0abe0aa441adff3bbf20bc49895b8f584453d837
viiviii <viiiix.viiviii@gmail.com>
2021-04-03 11:26:50 +0900

    Move equals() of Dollar to Money

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -7,10 +7,4 @@ class Dollar extends Money {
     Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
     }
-
-    @Override
-    public boolean equals(Object object) {
-        Money money = (Money) object;
-        return amount == money.amount;
-    }
 }
