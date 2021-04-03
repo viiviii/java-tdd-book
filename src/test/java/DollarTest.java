599f2ad936f00a2ac751e90804f860a4ecab3aee
viiviii <viiiix.viiviii@gmail.com>
2021-04-03 14:42:57 +0900

    Use Money instead of Dollar in testMultiplication()

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class DollarTest {
     @Test
     void testMultiplication() {
-        Dollar five = Money.dollar(5);
+        Money five = Money.dollar(5);
         assertEquals(new Dollar(10), five.times(2));
         assertEquals(new Dollar(15), five.times(3));
     }
