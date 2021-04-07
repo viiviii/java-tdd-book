54a68632ea79da5aec5150f472ed83b0bb6a1c92
viiviii <viiiix.viiviii@gmail.com>
2021-04-07 16:14:09 +0900

    Use Dollar factory method in Money

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -6,7 +6,7 @@ class Dollar extends Money {
     }
 
     Money times(int multiplier) {
-        return new Dollar(amount * multiplier);
+        return Money.dollar(amount * multiplier);
     }
 
 }
